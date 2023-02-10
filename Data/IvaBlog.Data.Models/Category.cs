9725661bfc3c5a855d2903be05b01f27b54c602d
DimitrovYordan <y.dimitrov84@abv.bg>
2023-02-10 16:57:08 +0200

    Create file Models with Category, Image, Ingredient, Recipe, RecipeIngredient classes.

---
+++ Data/IvaBlog.Data.Models/Category.cs
@@ -0,0 +1,18 @@
+﻿namespace IvaBlog.Data.Models
+{
+    using System.Collections.Generic;
+
+    using IvaBlog.Data.Common.Models;
+
+    public class Category : BaseDeletableModel<int>
+    {
+        public Category()
+        {
+            this.Recipes = new HashSet<Recipe>();
+        }
+
+        public string Name { get; set; }
+
+        public ICollection<Recipe> Recipes { get; set; }
+    }
+}
