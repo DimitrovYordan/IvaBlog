98ac38f99d7c70f0e72033010898920ed493f3a7
DimitrovYordan <y.dimitrov84@abv.bg>
2023-02-10 17:03:23 +0200

    Create CategoriesSeeder.

---
+++ Data/IvaBlog.Data/Seeding/CategoriesSeeder.cs
@@ -0,0 +1,25 @@
+﻿namespace IvaBlog.Data.Seeding
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using IvaBlog.Data.Models;
+
+    public class CategoriesSeeder : ISeeder
+    {
+        public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            if (dbContext.Categories.Any())
+            {
+                return;
+            }
+
+            await dbContext.Categories.AddAsync(new Category { Name = "Сладкиши"});
+            await dbContext.Categories.AddAsync(new Category { Name = "Пилешко"});
+            await dbContext.Categories.AddAsync(new Category { Name = "Свинско"});
+
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
