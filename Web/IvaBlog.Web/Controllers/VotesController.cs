22f8a67dccd1a03c414e9c2390cc09b77912804b
DimitrovYordan <y.dimitrov84@abv.bg>
2023-02-20 11:28:59 +0200

    Add controller for votes.

---
+++ Web/IvaBlog.Web/Controllers/VotesController.cs
@@ -0,0 +1,34 @@
+﻿namespace IvaBlog.Web.Controllers
+{
+    using System.Security.Claims;
+    using System.Threading.Tasks;
+
+    using IvaBlog.Services.Data;
+    using IvaBlog.Web.ViewModels.Votes;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VotesController : BaseController
+    {
+        private readonly IVotesService votesService;
+
+        public VotesController(IVotesService votesService)
+        {
+            this.votesService = votesService;
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<ActionResult<PostVoteResponseModel>> Post(PostVoteInputModel input)
+        {
+            var userId = this.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            await this.votesService.SetVoteAsync(input.RecipeId, userId, input.Value);
+            var averageVotes = this.votesService.GetAverageVotes(input.RecipeId);
+
+            return new PostVoteResponseModel { AverageVote = averageVotes };
+        }
+    }
+}
