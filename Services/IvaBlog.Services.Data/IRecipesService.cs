cfc247ed4811e1ea7b13ec219a5a5f1acf48916d
DimitrovYordan <y.dimitrov84@abv.bg>
2023-02-22 13:14:37 +0200

    Small changes.

--- Services/IvaBlog.Services.Data/IRecipesService.cs
+++ Services/IvaBlog.Services.Data/IRecipesService.cs
@@ -1,11 +1,18 @@
 ﻿namespace IvaBlog.Services.Data
 {
+    using System.Collections.Generic;
     using System.Threading.Tasks;
 
     using IvaBlog.Web.ViewModels.RecipeViewModel;
 
     public interface IRecipesService
     {
-        Task CreateAsync(CreateRecipeInputModel input);
+        Task CreateAsync(CreateRecipeInputModel input, string userId, string imagePath);
+
+        IEnumerable<T> GetAll<T>(int page, int itemsPerPage);
+
+        int GetCount();
+
+        T GetById<T>(int id);
     }
 }
