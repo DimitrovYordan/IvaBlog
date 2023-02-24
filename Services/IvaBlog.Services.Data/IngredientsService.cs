d369fcc9a45951a3f534cf3541b5228f8e3dc8af
DimitrovYordan <y.dimitrov84@abv.bg>
2023-02-24 10:17:30 +0200

    Implement IngredientsService and IIngredientsService for searching by ingredients.

---
+++ Services/IvaBlog.Services.Data/IngredientsService.cs
@@ -0,0 +1,24 @@
+﻿namespace IvaBlog.Services.Data
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using IvaBlog.Data.Common.Repositories;
+    using IvaBlog.Data.Models;
+    using IvaBlog.Services.Mapping;
+
+    public class IngredientsService : IIngredientsService
+    {
+        private readonly IDeletableEntityRepository<Ingredient> ingredientRepository;
+
+        public IngredientsService(IDeletableEntityRepository<Ingredient> ingredientRepository)
+        {
+            this.ingredientRepository = ingredientRepository;
+        }
+
+        public IEnumerable<T> GetAll<T>()
+        {
+            return this.ingredientRepository.All().OrderBy(x => x.Recipes).To<T>().ToList();
+        }
+    }
+}
