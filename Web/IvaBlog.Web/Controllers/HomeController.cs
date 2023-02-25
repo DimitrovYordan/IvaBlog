00acfa1cb351b00294b455c7d74bc5c452b540a0
DimitrovYordan <y.dimitrov84@abv.bg>
2023-02-25 09:36:24 +0200

    Implement random recipe.

--- Web/IvaBlog.Web/Controllers/HomeController.cs
+++ Web/IvaBlog.Web/Controllers/HomeController.cs
@@ -5,15 +5,20 @@
     using IvaBlog.Services.Data;
     using IvaBlog.Web.ViewModels;
     using IvaBlog.Web.ViewModels.Home;
+
     using Microsoft.AspNetCore.Mvc;
 
     public class HomeController : BaseController
     {
         private readonly IGetCountsService countsService;
+        private readonly IRecipesService recipesService;
 
-        public HomeController(IGetCountsService countsService)
+        public HomeController(
+            IGetCountsService countsService,
+            IRecipesService recipesService)
         {
             this.countsService = countsService;
+            this.recipesService = recipesService;
         }
 
         public IActionResult Index()
@@ -24,6 +29,7 @@
                 CategoriesCount = countsDto.CategoriesCount,
                 IngredientsCount = countsDto.IngredientsCount,
                 RecipesCount = countsDto.RecipesCount,
+                RandomRecipes = this.recipesService.GetRandom<IndexPageRecipeViewModel>(10),
             };
 
             return this.View(viewModel);
