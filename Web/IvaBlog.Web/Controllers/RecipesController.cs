11b5e5c66ddea80b7aed49ceb8c3bb2af2d18d0e
DimitrovYordan <y.dimitrov84@abv.bg>
2023-02-25 09:36:42 +0200

    Add delete method.

--- Web/IvaBlog.Web/Controllers/RecipesController.cs
+++ Web/IvaBlog.Web/Controllers/RecipesController.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Threading.Tasks;
 
+    using IvaBlog.Common;
     using IvaBlog.Data.Models;
     using IvaBlog.Services.Data;
     using IvaBlog.Web.ViewModels.Recipes;
@@ -32,13 +33,39 @@
             this.environment = environment;
         }
 
+        [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+        public IActionResult Edit(int id)
+        {
+            var inputModel = this.recipesService.GetById<EditRecipeInputModel>(id);
+            inputModel.CategoriesItem = this.categoriesService.GetAllAsKeyValuePairs();
+
+            return this.View(inputModel);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+        public async Task<IActionResult> Edit(int id, EditRecipeInputModel input)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                input.CategoriesItem = this.categoriesService.GetAllAsKeyValuePairs();
+
+                return this.View(input);
+            }
+
+            await this.recipesService.UpdateAsync(id, input);
+
+            // nameof will change automatically ById if we change it
+            return this.RedirectToAction(nameof(this.ById), new { id });
+        }
+
         [Authorize]
         public IActionResult Create()
         {
             var viewModel = new CreateRecipeInputModel();
             viewModel.CategoriesItem = this.categoriesService.GetAllAsKeyValuePairs();
 
-            return this.View();
+            return this.View(viewModel);
         }
 
         [HttpPost]
@@ -48,7 +75,8 @@
             if (!this.ModelState.IsValid)
             {
                 input.CategoriesItem = this.categoriesService.GetAllAsKeyValuePairs();
-                return this.View();
+
+                return this.View(input);
             }
 
             // This will take userId from Claims, will not work DB
@@ -62,14 +90,24 @@
             catch (Exception ex)
             {
                 this.ModelState.AddModelError(string.Empty, ex.Message);
+                input.CategoriesItem = this.categoriesService.GetAllAsKeyValuePairs();
+
+                return this.View(input);
             }
 
-            return this.Redirect("/");
+            this.TempData["Message"] = "Recipe added successfully.";
+
+            return this.Redirect("All");
         }
 
         public IActionResult All(int id = 1)
         {
             const int ItemsPerPage = 12;
+
+            if (id <= 0)
+            {
+                return this.NotFound();
+            }
 
             var viewModel = new RecipeListViewModel
             {
@@ -79,7 +117,7 @@
                 Recipes = this.recipesService.GetAll<RecipeInListViewModel>(id, ItemsPerPage),
             };
 
-            return this.View();
+            return this.View(viewModel);
         }
 
         public IActionResult ById(int id)
@@ -87,6 +125,15 @@
             var recipe = this.recipesService.GetById<SingleRecipeViewModel>(id);
 
             return this.View(recipe);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await this.recipesService.DeleteAsync(id);
+
+            return this.RedirectToAction(nameof(this.All));
         }
     }
 }
