52ef06f2698949153835303d7b3c03a1ef7d1780
DimitrovYordan <y.dimitrov84@abv.bg>
2023-02-26 08:42:18 +0200

    Add models for search by ingredients.

---
+++ Web/IvaBlog.Web.ViewModels/SearchRecipes/SearchIndexViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace IvaBlog.Web.ViewModels.SearchRecipes
+{
+    using System.Collections.Generic;
+
+    public class SearchIndexViewModel
+    {
+        public IEnumerable<IngredientNameIdViewModel> Ingredients { get; set; }
+    }
+}
