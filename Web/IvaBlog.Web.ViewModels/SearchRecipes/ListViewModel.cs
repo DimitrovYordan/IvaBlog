d71131ee8d4b7b7fefdfa4066d4a0de7cd61eb86
DimitrovYordan <y.dimitrov84@abv.bg>
2023-02-26 08:40:52 +0200

    Add ListViewModel.

---
+++ Web/IvaBlog.Web.ViewModels/SearchRecipes/ListViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace IvaBlog.Web.ViewModels.SearchRecipes
+{
+    using System.Collections.Generic;
+
+    using IvaBlog.Web.ViewModels.Recipes;
+
+    public class ListViewModel
+    {
+        public IEnumerable<RecipeInListViewModel> Recipes { get; set; }
+    }
+}
