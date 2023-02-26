48e2a768cd6711e67bd4b9edacb7c0325677490c
DimitrovYordan <y.dimitrov84@abv.bg>
2023-02-26 08:39:54 +0200

    Add IngredientNameIdViewModel.

---
+++ Web/IvaBlog.Web.ViewModels/SearchRecipes/IngredientNameIdViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace IvaBlog.Web.ViewModels.SearchRecipes
+{
+    using IvaBlog.Data.Models;
+    using IvaBlog.Services.Mapping;
+
+    public class IngredientNameIdViewModel : IMapFrom<Ingredient>
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
