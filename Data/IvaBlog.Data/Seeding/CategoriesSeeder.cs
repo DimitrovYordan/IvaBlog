21112cf133662d5dcf4a67c0711b0ad28e147750
DimitrovYordan <y.dimitrov84@abv.bg>
2023-03-01 10:23:04 +0200

    Delete test categories.

--- Data/IvaBlog.Data/Seeding/CategoriesSeeder.cs
+++ Data/IvaBlog.Data/Seeding/CategoriesSeeder.cs
@@ -4,8 +4,6 @@
     using System.Linq;
     using System.Threading.Tasks;
 
-    using IvaBlog.Data.Models;
-
     public class CategoriesSeeder : ISeeder
     {
         public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
@@ -14,10 +12,6 @@
             {
                 return;
             }
-
-            await dbContext.Categories.AddAsync(new Category { Name = "Сладкиши" });
-            await dbContext.Categories.AddAsync(new Category { Name = "Пилешко" });
-            await dbContext.Categories.AddAsync(new Category { Name = "Свинско" });
 
             await dbContext.SaveChangesAsync();
         }
